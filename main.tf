1edf0155ae94571bdbc50f25670962df969000f1
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-12-31 13:47:50 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+provider "aws" {
+  region = "us-east-1" # Change this to your desired region
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-079db87dc4c10ac91" # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name="clarusway-kp"
+  tags = {
+    Name = "Web Server of Bookstore"
+  }
+
+   user_data = <<-EOF
+              #!/bin/bash
+              git clone https://github.com/your-username/your-repo.git /home/ec2-user/bookstore
+              cd /home/ec2-user/bookstore
+              docker-compose up -d
+              EOF
+}
+
+output "bookstore_api_url" {
+  value = aws_instance.web_server.public_ip
+}
+
+
+
+
+
